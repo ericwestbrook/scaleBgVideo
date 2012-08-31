9188ddf4a05379e9cf61f93d3ca50496f8f50a61
Eric Westbrook <eric@ericwestbrook.net>
2012-08-31 12:35:42 -0700

    adding all resources

--- scaleBgVideo.js
+++ scaleBgVideo.js
@@ -1,12 +1,16 @@
 function styleVideo() {
   var target = '#bgVideo';
+  $('body').css({'width' : '100%', 'height' : '100%', 'overflow-x' : 'hidden'});
+  $(target).wrap('<div id="videoWrap" />');
+  $('#videoWrap').css({'width' :'100%', 'height' : '100%', 'top' : '0', 'left' : '0', 'overflow-x' : 'hidden', 'z-index' : '-1'});
   if ($(target).hasClass('staticBg')) {
-    $(target).css({'position' : 'static'});
+    $('#videoWrap').css({'position' : 'static'});
   }
   else {
-    $(target).css({'position' : 'absolute'});
+    $('#videoWrap').css({'position' : 'absolute'});
   }
   $(target).css({'display' : 'block', 'height' : '100%', 'margin' : '0 auto', 'overflow-x' : 'hidden'});
+  $(target).get(0).play();
 }
 
 //Scales background videos to browser window
